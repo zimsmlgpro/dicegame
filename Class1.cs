2b0f3969bbbd993bcb64ad62fd4c66573b19c0d6
Brandon <zimsmlgpro@gmail.com>
2025-01-15 17:10:33 -0500

    Added the functionality for the user to guess the number and get feedback based on the result

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceGame
+{
+    internal class Class1
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random random = new Random();  // Runs at the beginning every time
+            int diceRoll = random.Next(1, 6);  // Get a number between 1 and 6
+
+            Console.WriteLine($"Guess the roll of the dice? (A number between 1 and 6):");  // Asks for the number from the user
+            int userGuess = Convert.ToInt32(Console.ReadLine()); //Takes users guess and converts it to a integer
+
+            if (userGuess == diceRoll)
+            {
+                Console.WriteLine("You win! The dice roll was " + diceRoll + ".");
+            }
+            else
+            {
+                Console.WriteLine("You're a fucking moron. The number was " + diceRoll + ". Go read a book, you troglodyte");
+            }
         }
     }
 }
+    
+
