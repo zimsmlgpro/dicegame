530264f8959967636e6a7ca3fd798a5455d5333c
Brandon <zimsmlgpro@gmail.com>
2025-01-16 11:11:10 -0500

    Added functionality to restart the game or press any other key to exit.

--- Program.cs
+++ Program.cs
@@ -4,19 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Random random = new Random();  // Runs at the beginning every time
-            int diceRoll = random.Next(1, 6);  // Get a number between 1 and 6
+            bool playAgain = true; //Setting the variable to run this until it is set to false
 
-            Console.WriteLine($"Guess the roll of the dice? (A number between 1 and 6):");  // Asks for the number from the user
-            int userGuess = Convert.ToInt32(Console.ReadLine()); //Takes users guess and converts it to a integer
+            while (playAgain)
+            {
+                Random random = new Random();  // Runs at the beginning every time
+                int diceRoll = random.Next(1, 6);  // Get a number between 1 and 6
 
-            if (userGuess == diceRoll)
-            {
-                Console.WriteLine("You win! The dice roll was " + diceRoll + ".");
-            }
-            else
-            {
-                Console.WriteLine("You're a fucking moron. The number was " + diceRoll + ". Go read a book, you troglodyte");
+                Console.WriteLine($"Guess the roll of the dice? (A number between 1 and 6):");  // Asks for the number from the user
+                int userGuess = Convert.ToInt32(Console.ReadLine()); //Takes users guess and converts it to a integer
+
+                if (userGuess == diceRoll)
+                {
+                    Console.WriteLine("You win! The dice roll was " + diceRoll + ".");
+                }
+                else
+                {
+                    Console.WriteLine("You're a fucking moron. The number was " + diceRoll + ". Go read a book, you troglodyte");
+                }
+
+                Console.WriteLine("Press 'R' to play again or any other key to quit.");
+                string userChoice = Console.ReadLine(); //Get user input
+
+                if (userChoice.ToLower() != "r") //If the user doesn't press any variation of r
+                {
+                    playAgain = false; //Stops the whole loop
+                    Console.WriteLine("Thanks for giving my Dice Game a try!");
+                }
             }
         }
     }
